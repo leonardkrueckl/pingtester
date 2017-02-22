9ab568e216d760dfad884d8a859105e7e66791b7
Leonard Krueckl <kru16243@spengergasse.at>
2017-02-22 01:17:18 +0100

    WPF Functionality Complete
    Every second the prorgam gives you your current and average ping to the
    Google servers

--- pingerwpfv2/MainWindow.xaml.cs
+++ pingerwpfv2/MainWindow.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.NetworkInformation;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace pingerwpfv2
 {
@@ -26,57 +17,72 @@ namespace pingerwpfv2
     {
         static List<int> pings = new List<int>();
         static string filename = DateTime.Today.Year + "-" + DateTime.Today.Month + "-" + DateTime.Today.Day + "-" + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Second + ".txt";
+        StreamWriter sw = File.AppendText(filename);
+        int pingmedian = 0;
 
 
         public MainWindow()
         {
             InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
         }
 
-        public static double PingTimeAverage(string host, int echoNum)
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            double ping = PingTimeAverage("8.8.8.8");
+            if (ping != 1337420) lPing.Content = "" + ping;
+            else lPing.Content = "Connection timed out";
+
+            if (ping < 70) lPing.Foreground = Brushes.White;
+            else if (ping > 70 && ping < 100) lPing.Foreground = Brushes.Yellow;
+            else if (ping > 100) lPing.Foreground = Brushes.Red;
+
+            sw.WriteLine(ping);
+            sw.Flush();
+
+            pings.Add((int)ping);
+            for(int i = 0; i < pings.Count; i++)
+            {
+                pingmedian += pings[i];
+            }
+
+            pingmedian = pingmedian / pings.Count;
+            lPingMedian.Content = pingmedian;
+
+            if (pingmedian < 70) lPingMedian.Foreground = Brushes.White;
+            else if (pingmedian > 70 && pingmedian < 100) lPingMedian.Foreground = Brushes.Yellow;
+            else if (pingmedian > 100) lPingMedian.Foreground = Brushes.Red;
+            
+        }
+
+        public static double PingTimeAverage(string host)
         {
             long totalTime = 0;
             Ping pingSender = new Ping();
 
-            for (int i = 0; i < echoNum; i++)
+           
+            PingReply reply = pingSender.Send(host);
+            if (reply.Status == IPStatus.Success)
             {
-                PingReply reply = pingSender.Send(host);
-                if (reply.Status == IPStatus.Success)
-                {
-                    totalTime += reply.RoundtripTime;
-                }
-                else {
-                    Console.WriteLine("Connection timed out");
-                    totalTime = 1337;
-                }
+                totalTime += reply.RoundtripTime;
             }
-            return totalTime / echoNum;
-        }
-
-        private void OnStartup(object sender, StartupEventArgs e)
-        {
-            lPing.Content = ""+PingTimeAverage("8.8.8.8", 1);
-        }
-
-
-        private void doPings()
-        {
-            StreamWriter sw = File.AppendText(filename);
-
-            while (true)
-            {
-                double ping = PingTimeAverage("8.8.8.8", 1);
-                lPing.Content = "" + ping;
-                sw.WriteLine(ping);
-                pings.Add((int)ping);
-                sw.Flush();
-                Thread.Sleep(800);
+            else {
+                totalTime = 1337420;
             }
+            return totalTime;
         }
 
-        private void lPing_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
         {
-            doPings();
+            //do my stuff before closing
+
+            base.OnClosing(e);
         }
+
     }
 }
