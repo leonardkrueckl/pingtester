9b64b9aa8fe583ee8756f05ffd80ec05a52815a2
Leonard Krueckl <kru16243@spengergasse.at>
2017-02-23 19:52:20 +0100

    Added Runtime, Worst Ping, Best Ping and Jumps
    Runtime, best and worst ping are self explanatory, jumps is the total of
    times when the ping jumped more than 30 ms of the last ping

--- pingerwpfv2/MainWindow.xaml.cs
+++ pingerwpfv2/MainWindow.xaml.cs
@@ -17,49 +17,89 @@ namespace pingerwpfv2
     {
         static List<int> pings = new List<int>();
         int pinger = 0;
-        int pingadder = 0;
+        long pingadder = 0;
         static string filename = DateTime.Today.Year + "-" + DateTime.Today.Month + "-" + DateTime.Today.Day + "-" + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Second + ".txt";
+
+        DateTime begin = DateTime.Now;
+
         StreamWriter sw = File.AppendText(filename);
         int pingmedian = 0;
-        double ping = 0;
+        int pingjumps=0;
+        int lastping=1337420;
 
 
         public MainWindow()
         {
             InitializeComponent();
 
+            DispatcherTimer timer2 = new DispatcherTimer();
+            timer2.Interval = TimeSpan.FromSeconds(15);
+            timer2.Tick += timer2_Tick;
+            timer2.Start();
+
+
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(800);
             timer.Tick += timer_Tick;
             timer.Start();
+
+            TimeSpan duration = DateTime.Now.Subtract(begin);
+            lDuration.Content = duration.Hours + ":" + duration.Minutes + ":" + duration.Seconds;
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
             try {
                 double ping = PingTimeAverage("8.8.8.8");
+                pings.Add((int)ping);
                 if (ping != 1337420) lPing.Content = ping;
-                else lPing.Content = "Connection timed out";
+                else lPing.Content = "Connection timeout";
                 sw.WriteLine(ping);
                 sw.Flush();
 
-                pingadder += (int)ping;
+                if (ping>lastping+30) 
+                {
+                    pingjumps++;
+                }
+                lPingJumps.Content = pingjumps;
+                
+                pingadder += (long)ping;
                 pinger++;
-                pingmedian = pingadder / pinger;
+                pingmedian = (int)(pingadder / pinger);
                 lPingMedian.Content = pingmedian;
 
-                if (ping < 70) lPing.Foreground = Brushes.White;
-                else if (ping > 70 && ping < 100) lPing.Foreground = Brushes.Yellow;
-                else if (ping > 100) lPing.Foreground = Brushes.Red;
-                if (pingmedian < 70) lPingMedian.Foreground = Brushes.White;
-                else if (pingmedian > 70 && pingmedian < 100) lPingMedian.Foreground = Brushes.Yellow;
-                else if (pingmedian > 100) lPingMedian.Foreground = Brushes.Red;
-
+                ChangeColorOfLabel(lPing, ping);
+                ChangeColorOfLabel(lPingMedian, pingmedian);
+                lastping = (int)ping;
             }
             catch (Exception end)
             {
                 MessageBox.Show(end.Message);
+                Thread.Sleep(1000);
+                base.Close();
             }            
+        }
+
+        private void ChangeColorOfLabel(System.Windows.Controls.Label lab, double ping)
+        {
+            if (ping < 50) lab.Foreground = Brushes.Green;
+            else if (ping < 70) lab.Foreground = Brushes.White;
+            else if (ping > 70 && ping < 100) lab.Foreground = Brushes.Yellow;
+            else if (ping > 100) lab.Foreground = Brushes.Red;
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(pings.IndexOf(1337420) != -1) pings.Remove(1337420);
+
+            List<int> pinglist = pings;
+            pinglist.Sort();
+            lPingBest.Content = pinglist[0];
+            lPingWorst.Content = pinglist[pinglist.Count - 1];
+
+            TimeSpan duration = DateTime.Now.Subtract(begin);
+            lDuration.Content = duration.Hours + ":" + duration.Minutes + ":" + duration.Seconds;
         }
 
         public static double PingTimeAverage(string host)
@@ -73,22 +113,21 @@ namespace pingerwpfv2
             {
                 totalTime += reply.RoundtripTime;
             }
-            else {
-                totalTime = 1337420;
-            }
+            else totalTime = 1337420;
+
             return totalTime;
         }
-
-
+        
         protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
         {
-            sw.WriteLine("------------------------------------------------------");
-            sw.WriteLine(pingmedian);
+            TimeSpan duration = DateTime.Now.Subtract(begin);
+
+            sw.WriteLine("-----------------");
+            sw.WriteLine("Average: " +pingmedian);
             sw.Flush();
 
-            Thread.Sleep(500);
+            
             base.OnClosing(e);
         }
-
     }
 }
